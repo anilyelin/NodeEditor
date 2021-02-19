11a67ed71bae4bd3e6ada40c8e728d2bfa6d451e
anilyelin <dsadsad>
2021-02-19 03:07:52 +0100

    Initial py files

--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
-import PyQt5.QtCore 
-print("Hello")
+import sys
+from PyQt5.QtWidgets import *
+from node_editor_wnd import NodeEditorWnd
+
+if __name__=="__main__":
+    app = QApplication(sys.argv)
+    wnd = NodeEditorWnd()
+    sys.exit(app.exec_())
